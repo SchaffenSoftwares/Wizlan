b2343335f348c0314fa316441d3130bb2d28dc7e
shreyasbhakta <shreyasbhakta@gmail.com>
2020-09-09 20:06:34 +0530

    UI updated

--- app/src/main/java/com/example/wizlan/HomeFragment.kt
+++ app/src/main/java/com/example/wizlan/HomeFragment.kt
@@ -1,14 +1,23 @@
 package com.example.wizlan
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
 
+    private var QrScanner: CardView? = null
+    private var QrGenerator: CardView? = null
+    private var PdfScanner: CardView? = null
+    private var Trans: CardView? = null
 
 
     override fun onCreateView(
@@ -16,8 +25,33 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Trans = view.findViewById(R.id.cvtexttranslate)
+        PdfScanner = view.findViewById(R.id.cvpdfscanner)
+        QrScanner = view.findViewById(R.id.cvqrscanner)
+        QrGenerator = view.findViewById(R.id.cvqrgenerator)
+
+        PdfScanner!!.setOnClickListener {
+            startActivity(Intent(activity, pdfScanner::class.java))
+        }
+        QrGenerator!!.setOnClickListener {
+            startActivity(Intent(activity, qrGenerator::class.java))
+        }
+        QrScanner!!.setOnClickListener {
+            startActivity(Intent(activity,qrScanner::class.java))
+        }
+        Trans!!.setOnClickListener {
+            startActivity(Intent(activity,Translator::class.java))
+        }
+
+        return view
+
+
     }
+
+
+
 
 
 }
---
+++ app/src/main/java/com/example/wizlan/pdfScanner.kt
@@ -0,0 +1,11 @@
+package com.example.wizlan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pdfScanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_scanner)
+    }
+}
