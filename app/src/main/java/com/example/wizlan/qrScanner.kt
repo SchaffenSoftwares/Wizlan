ed23779d339f5b1dd27700c5ded1001d006337d9
shreyasbhakta <shreyasbhakta@gmail.com>
2020-09-11 21:42:23 +0530

    scanner added

--- app/src/main/java/com/example/wizlan/qrScanner.kt
+++ app/src/main/java/com/example/wizlan/qrScanner.kt
@@ -1,11 +1,47 @@
 package com.example.wizlan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class qrScanner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scanner)
+
+
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+        val scanner = IntentIntegrator(this)
+        scanner.initiateScan()
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        scanner.setBeepEnabled(false)
+        return super.onCreateView(name, context, attrs)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            }
+        }else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+
     }
 }
