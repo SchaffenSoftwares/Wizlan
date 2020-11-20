40298ffa52137124e68563c279fcd3abe51f4482
shreyasbhakta <shreyasbhakta@gmail.com>
2020-11-20 23:14:09 +0530

    minor bugs fixing and ui improvement

---
+++ app/src/main/java/com/example/wizlan/models/translation.kt
@@ -0,0 +1,10 @@
+package com.example.wizlan.models
+
+import com.google.gson.annotations.SerializedName
+
+
+class Translation{
+    var lan:String? =null
+    var msg:String? =null
+    var result:String? =null
+}
--- app/src/main/java/com/example/wizlan/utilityclasses/Constants.kt
+++ app/src/main/java/com/example/wizlan/utilityclasses/Constants.kt
@@ -11,5 +11,18 @@ class Constants {
             "One click away to know the unknown"
         )
         val tagLineSize = tagLines.size
+
+        val language = arrayOf(
+            "Kannada","Japanese","Hindi","Telugu","Tamil", "Afrikaans","Albanian",
+            "Arabic", "Amharic", "Azerbaijani", "Bengali", "Bulgarian", "Catalan",
+            "Chichewa", "Chinese(simplified)", "Chinese(traditional)", "Corsican", "Croatian",
+            "Czech", "Danish", "Dutch", "Esperanto", "Finnish", "French", "Galician","Georgian", "German",
+        "Greek", "Gujrati", "Hausa", "Hawaiin", "Hebrew", "Hmong", "Indonesian", "Italian", "Kannada",
+            "Kazakh", "Lao", "Latin", "Lithuanian", "Luxembourgish", "Malagasy", "Malayalam", "Marathi", "Mongolian",
+            "Myanmar", "Nepali", "Norwegian", "Odia", "Persian", "Polish", "Portuguese", "Punjabi",
+            "Romanian", "Russian", "Serbian", "Sindhi", "Slovak", "Somali", "Spanish", "Swahili", "Swedish",
+            "Thai", "Turkish", "Urdu", "Uzbek", "Vietnamese", "Yiddish", "Zulu",
+        )
+        val languageSize = language.size
     }
 }
